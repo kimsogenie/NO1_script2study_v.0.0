10772b833a31a0890551954a811c76a5987fd86c
kimsogenie <kimsogenie@gmail.com>
2026-03-28 13:19:45 +0900

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-
 export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-SKMH27X4ZX" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SKMH27X4ZX');
+            `,
+          }}
+        />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#4A90D9" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
